1a0f29a31db7a12c60ee73bd2cd06cca170b878d
isilasa <isilasaquitall@gmail.com>
2020-02-11 16:45:48 +0300

    v.1.0.0

--- AccessPoint/dbData.xaml.cs
+++ AccessPoint/dbData.xaml.cs
@@ -20,88 +20,145 @@ namespace AccessPoint
             MySqlConnection connection = new MySqlConnection("server = localhost;port = 3306; username = root; password = 1123581321Bkmz ; database = accesspoint");
             DataTable table = new DataTable();
 
-            if (tableBox.Text == "Кабинеты")
+            
+            if(tableBox.Text != "Таблицы" && categories.Text == "Категории")//add conditions
             {
-                MySqlCommand command = new MySqlCommand("SELECT * FROM `room` ", connection);
+                if (tableBox.Text == "Кабинеты")
+                {
+                    MySqlCommand command = new MySqlCommand("SELECT * FROM room ", connection);
 
-                connection.Open();
+                    connection.Open();
 
-                MySqlDataReader reader = command.ExecuteReader();
-                table.Load(reader);
-                dbDataGrid.AutoGenerateColumns = true;
-                dbDataGrid.ItemsSource = table.DefaultView;
+                    MySqlDataReader reader = command.ExecuteReader();
+                    table.Load(reader);
+                    dbDataGrid.AutoGenerateColumns = true;
+                    dbDataGrid.ItemsSource = table.DefaultView;
 
-                connection.Close();
+                    connection.Close();
+                }
+                if (tableBox.Text == "Кафедры")
+                {
+                    MySqlCommand command = new MySqlCommand("SELECT * FROM cathedras ", connection);
+
+                    connection.Open();
+
+                    MySqlDataReader reader = command.ExecuteReader();
+                    table.Load(reader);
+                    dbDataGrid.AutoGenerateColumns = true;
+                    dbDataGrid.ItemsSource = table.DefaultView;
+
+                    connection.Close();
+                }
+                if (tableBox.Text == "Название Абонентского пункта")
+                {
+                    MySqlCommand command = new MySqlCommand("SELECT name,number_of_room,categories,number_of_cathedras,date,isProtected " +
+                        "                                    FROM name_of_access_point " +
+                        "                                    LEFT JOIN cathedras ON name_of_access_point.cathedras_idcathedras = cathedras.idcathedras " +
+                        "                                    LEFT JOIN is_protected_access_point ON name_of_access_point.is_protected_access_point_idis_protected_access_point = is_protected_access_point.idis_protected_access_point " +
+                        "                                    LEFT JOIN installation_date ON name_of_access_point.installation_date_idinstallation_date = installation_date.idinstallation_date " +
+                        "                                    LEFT JOIN room ON name_of_access_point.Room_idRoom = room.idRoom " +
+                        "                                    LEFT JOIN categories ON room.categories_idcategories = categories.idcategories", connection);
+
+                    connection.Open();
+
+                    MySqlDataReader reader = command.ExecuteReader();
+                    table.Load(reader);
+                    dbDataGrid.AutoGenerateColumns = true;
+                    dbDataGrid.ItemsSource = table.DefaultView;
+
+                    connection.Close();
+                }
+                if (tableBox.Text == "Защищенность")
+                {
+                    MySqlCommand command = new MySqlCommand("SELECT * FROM is_protected_access_point ", connection);
+
+                    connection.Open();
+
+                    MySqlDataReader reader = command.ExecuteReader();
+                    table.Load(reader);
+                    dbDataGrid.AutoGenerateColumns = true;
+                    dbDataGrid.ItemsSource = table.DefaultView;
+
+                    connection.Close();
+                }
+                if (tableBox.Text == "Дата установки")
+                {
+                    MySqlCommand command = new MySqlCommand("SELECT * FROM installation_date ", connection);
+
+                    connection.Open();
+
+                    MySqlDataReader reader = command.ExecuteReader();
+                    table.Load(reader);
+                    dbDataGrid.AutoGenerateColumns = true;
+                    dbDataGrid.ItemsSource = table.DefaultView;
+
+                    connection.Close();
+                }
+                if (tableBox.Text == "Категории")
+                {
+                    MySqlCommand command = new MySqlCommand("SELECT * FROM categories ", connection);
+
+                    connection.Open();
+
+                    MySqlDataReader reader = command.ExecuteReader();
+                    table.Load(reader);
+                    dbDataGrid.AutoGenerateColumns = true;
+                    dbDataGrid.ItemsSource = table.DefaultView;
+
+                    connection.Close();
+                }
+                //if (tableBox.Text == "Таблицы")
+                //{
+                //    MessageBox.Show("Chooce conditions!","Warning");
+                //}
             }
-            if(tableBox.Text == "Кафедры")
+            else
             {
-                MySqlCommand command = new MySqlCommand("SELECT * FROM `cathedras` ", connection);
+                if ((roomName.Text == "" && kafedrName.Text == "")||(roomName.Text == "room" && kafedrName.Text == "kafedras") || (roomName.Text == "" && kafedrName.Text == "kafedras") || (roomName.Text == "room" && kafedrName.Text == ""))
+                {
+                    MySqlCommand command = new MySqlCommand("SELECT name,number_of_room,categories " +
+                        "                                    FROM name_of_access_point " +
+                        "                                    LEFT JOIN room ON name_of_access_point.idtable1 = room.idRoom " +
+                        "                                    LEFT JOIN categories ON room.categories_idcategories = categories.idcategories" +
+                        "                                    WHERE categories = \"" + categories.Text + "\"", connection);
 
-                connection.Open();
+                    connection.Open();
 
-                MySqlDataReader reader = command.ExecuteReader();
-                table.Load(reader);
-                dbDataGrid.AutoGenerateColumns = true;
-                dbDataGrid.ItemsSource = table.DefaultView;
+                    MySqlDataReader reader = command.ExecuteReader();
+                    table.Load(reader);
+                    dbDataGrid.AutoGenerateColumns = true;
+                    dbDataGrid.ItemsSource = table.DefaultView;
 
-                connection.Close();
+                    connection.Close();
+                }
+                //if ()
+                //{
+                //    MySqlCommand command = new MySqlCommand("SELECT name,number_of_room,categories " +
+                //        "                                    FROM name_of_access_point " +
+                //        "                                    LEFT JOIN room ON name_of_access_point.idtable1 = room.idRoom " +
+                //        "                                    LEFT JOIN categories ON room.categories_idcategories = categories.idcategories" +
+                //        "                                    WHERE categories = \"" + categories.Text + "\"", connection);
+
+                //    connection.Open();
+
+                //    MySqlDataReader reader = command.ExecuteReader();
+                //    table.Load(reader);
+                //    dbDataGrid.AutoGenerateColumns = true;
+                //    dbDataGrid.ItemsSource = table.DefaultView;
+
+                //    connection.Close();
+                //}
             }
-            if (tableBox.Text == "Название Абонентского пункта")
-            {
-                MySqlCommand command = new MySqlCommand("SELECT `name`,`number_of_room`,`categories` FROM `name_of_access_point` LEFT JOIN `room` ON name_of_access_point.idtable1 = room.idRoom LEFT JOIN `categories` ON room.categories_idcategories = categories.idcategories", connection);
+        }
 
-                connection.Open();
+        private void roomName_PreviewMouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            roomName.Text = "";
+        }
 
-                 MySqlDataReader reader = command.ExecuteReader();
-                table.Load(reader);
-                dbDataGrid.AutoGenerateColumns = true;
-                dbDataGrid.ItemsSource = table.DefaultView;
-
-                connection.Close();
-            }
-            if (tableBox.Text == "Защищенность")
-            {
-                MySqlCommand command = new MySqlCommand("SELECT * FROM `is_protected_access_point` ", connection);
-
-                connection.Open();
-
-                MySqlDataReader reader = command.ExecuteReader();
-                table.Load(reader);
-                dbDataGrid.AutoGenerateColumns = true;
-                dbDataGrid.ItemsSource = table.DefaultView;
-
-                connection.Close();
-            }
-            if (tableBox.Text == "Дата установки")
-            {
-                MySqlCommand command = new MySqlCommand("SELECT * FROM `installation_date` ", connection);
-
-                connection.Open();
-
-                MySqlDataReader reader = command.ExecuteReader();
-                table.Load(reader);
-                dbDataGrid.AutoGenerateColumns = true;
-                dbDataGrid.ItemsSource = table.DefaultView;
-
-                connection.Close();
-            }
-            if (tableBox.Text == "Категории")
-            {
-                MySqlCommand command = new MySqlCommand("SELECT * FROM `categories` ", connection);
-
-                connection.Open();
-
-                MySqlDataReader reader = command.ExecuteReader();
-                table.Load(reader);
-                dbDataGrid.AutoGenerateColumns = true;
-                dbDataGrid.ItemsSource = table.DefaultView;
-
-                connection.Close();
-            }
-            if (tableBox.Text == "Таблицы")
-            {
-                MessageBox.Show("Chooce conditions!","Warning");
-            }
+        private void kafedrName_PreviewMouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            kafedrName.Text = "";
         }
     }
 }
